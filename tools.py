0c030cb4c456b71f0f9a23ad2dc1d123aa21b844
david <davidepanto@gmail.com>
2026-05-16 16:44:34 +0200

    tutto iniziò così (versione pulita)

--- tools.py
+++ tools.py
@@ -2,5 +2,5 @@ from langchain.tools import tool
 
 @tool
 def write_description(about: str, to: str, content: str):
-    """Write a description of microcontroller"""
+    """Write a description of microcontroller  """
     return f"My description of microcontroller: {about} to {to} is {content}"
